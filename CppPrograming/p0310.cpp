aaf162a71d14f7fa32c1819a9499e59b522616c3
Gtavexir <gtavexir@kookmin.ac.kr>
2021-03-11 15:04:39 +0900

    upload pratice code

---
+++ CppPrograming/p0310.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "enter one character and press enter key...\n";
+    cin >> ch;
+    cout << "*" << ch << "*" << endl;
+
+    return 0;
+}
