d86a0d47218ef06bbaf8f0fa4fa2b2ba9c0a00b5
Gtavexir <gtavexir@kookmin.ac.kr>
2021-03-15 23:48:33 +0900

    add other pratice code

--- CppPrograming/a10.cpp
+++ CppPrograming/a10.cpp
@@ -14,3 +14,22 @@ int main() {
 
     return 0;
 }
+
+// int main() {
+//     char v1, v2, v3, v4, v5, v6, v7, v8, v9, v10;
+
+//     v1 = 'a';
+//     v2 = 'a';
+//     v3 = 'a';
+//     v4 = 'a';
+//     v5 = 'a'; v6 = 'a'; v7 = 'a'; v8 = 'a'; v9 = 'a';
+//     v10 = 'a';
+
+//     cout << v1 << endl;
+//     cout << v2 << endl; cout << v3 << endl;
+//     cout << v4 << endl << v5 << endl << v6 << endl;
+//     cout << v7 << endl << v8 << endl << v9 << endl;
+//     cout << v10 << endl;
+
+//     return 0;
+// }
